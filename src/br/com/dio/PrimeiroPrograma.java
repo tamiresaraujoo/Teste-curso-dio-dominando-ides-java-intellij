81a54987d3b73c9ca5e2269dffcfb6803bce84c7
tamiresaraujoo <tamiresaraujoo@hotmail.com>
2022-07-16 18:22:12 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    static Gato gato = new Gato();
+
+    public static void main(String[] args) {
+        System.out.println(gato);
+    }
+     /*int a = 6;
+     int b = 3;
+
+        System.out.println("hello world!" + (a+b));
+    }*/
+}
+
